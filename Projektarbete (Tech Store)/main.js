a9ec4d4068dd410e31ae54606af964d65359b4bb
Your name hereOscarwarmark <oscar.warmark@gmail.com>
2022-10-19 15:43:24 +0200

    fixat css för startsida och knapp

--- Projektarbete (Tech Store)/main.js
+++ Projektarbete (Tech Store)/main.js
@@ -44,6 +44,15 @@ function addProductsToWebpage() {
     price.innerText=product.price + " kr";
     bildblock.appendChild(price);
 
+    const addbutton=document.createElement("button");
+
+    addbutton.addEventListener("click",()=>{
+
+    });
+    addbutton.className="btn btn-primary btn-xs"
+    bildblock.appendChild(addbutton);
+    addbutton.innerHTML= '<i class="fa fa-shopping-cart fa-1x" aria-hidden="true"></i> Lägg till i kundvagnen';
+
   }
     // to reduce complexity and increase readability. Each function should have
     // an explainetory comment like the one for this function, see row 22.
